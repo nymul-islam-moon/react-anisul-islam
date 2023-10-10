363b6a3a053d9792d39b379a1a8c8f5d2d82a1f9
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-10 15:43:42 +0600

    Class 8
    Props And Destructuring

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Card from "./components/Card";
 function App(){
     return <div>
         <h1 className={'headingStyle'}>TO DO APP</h1>
-        <Card />
+        <Card titleText={'This is the first card'} descText={'This is the description'}/>
+        <Card titleText={'This is second title'} descText={'This is the description'} />
+        <Card titleText={'This is the third card'} descText={'This is the description'} />
     </div>;
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,8 @@
-const todoTitle = 'Call Family';
-const todoDecsription = 'Test is the best uses for the life time example for for for.'
-
-function Card(){
+function Card(props){
+    const {titleText, descText} = props;
     return <div className={'card'}>
-        <h3 className={'cardtitle'}>{todoTitle}</h3>
-        <p className={'cardDesc'}>{todoDecsription}</p>
+        <h3 className={'cardtitle'}>{titleText}</h3>
+        <p className={'cardDesc'}>{descText}</p>
         <p className={'cardFooter'}>{new Date().getFullYear()}</p>
     </div>
 }
