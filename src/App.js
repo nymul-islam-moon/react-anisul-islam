4259b48629c10dfd7411109917f5740bf92ad425
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-10 16:04:06 +0600

    Class 9
    Data showing using for loop

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Card from "./components/Card";
 import Data from "./data.json"
 
 function App(){
-    console.log(Data);
-    return <div>
-        <h1 className={'headingStyle'}>TO DO APP</h1>
-        <Card titleText={Data[0].title} descText={Data[0].desc}/>
-        <Card titleText={Data[1].title} descText={Data[1].desc}/>
-        <Card titleText={Data[2].title} descText={Data[2].desc}/>
 
+    let items = [];
+
+    for ( let  i = 0; i < Data.length; i++ ) {
+        items.push( <Card titleText={ Data[i].title } descText={ Data[i].desc }/> )
+    }
+
+    return <div>
+        <h1 className={ 'headingStyle' }>TO DO APP</h1>
+        { items }
     </div>;
 }
 
