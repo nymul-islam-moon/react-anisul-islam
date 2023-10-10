02bcf254d12e552e956a523264e5adb9aa276563
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-10 13:04:57 +0600

    Inline Css adding system shown

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+const todoTitle = 'Call Family';
+const todoDecsription = 'Test is the best uses for the life time example for for for.'
+const headingStyle = {
+    backgroundColor : 'black',
+    color : 'cyan',
+    textAlign : 'center',
+    padding : '15px'
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>TO DO APP</h1>
+    <div>
+        <h1 style={headingStyle}>To DO APP</h1>
+        <h3>{todoTitle}</h3>
+        <p>{todoDecsription}</p>
+        <p>{new Date().getFullYear()}</p>
+    </div>
 );
