53e65a57381fd612aa8d2b204539070d70560087
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-11 17:46:34 +0600

    Class 16
    Multiple Create Element and Component under the hood

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+
+function Welcome () {
+    return <h1>Welcome</h1>;
+
+}
+
+function Welcome2 () {
+    return React.createElement( "h1", {}, "Welcome" );
+}
+
+function Todo () {
+    return <div>
+        <h3>Todo Title</h3>
+        <p>Todo Desc</p>
+    </div>
+}
+
+function Todo2 () {
+    return React.createElement( "div", {}, React.createElement( "p", {} , "Todo Title 2"),
+        React.createElement( "p", {} , "Todo Description 2")
+    );
+}
+
 function App() {
     return (
         <div>
-
+            <Welcome />
+            <Welcome2 />
+            <Todo />
+            <Todo2 />
         </div>
     )
 }
