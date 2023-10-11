bb491275f2f57005822d5ccef263553044a36330
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-11 18:16:34 +0600

    Class 18
    6.17 minute

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { FaAppStore } from "react-icons/fa6";
 
 function App() {
     return (
         <div>
-            <h1>Welcome TO React</h1>
+            <h1>Welcome TO React <FaAppStore /> </h1>
         </div>
     )
 }
