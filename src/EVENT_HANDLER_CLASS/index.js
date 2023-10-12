5d1f7c0c3fd6f6264cead575e6386a4f01ef0872
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-12 15:30:19 +0600

    Class 22
    Event Handler Done

--- src/EVENT_HANDLER_CLASS/index.js
+++ src/EVENT_HANDLER_CLASS/index.js
@@ -7,14 +7,28 @@ export default class EVENT_HANDLER_CLASS extends Component {
      * @returns {JSX.Element}
      */
 
-    handleClick = () => {
-        alert('Button Clicked')
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            changedValue : ''
+        }
+    }
+
+    handleChange = ( e ) => {
+        this.setState( {
+            changedValue : e.target.value
+        }, () => {
+            console.log( this.state.changedValue )  // It will show currect changed value on the console
+        } )
+        // console.log( this.state.changeddValue ) // if we console this we will see the value but one charecter behind cause this is not active on change function
     }
 
     render() {
         return (
             <div>
-                <button onClick={this.handleClick}> Click Here </button>
+                <input type="text" onChange={    this.handleChange} />
+                <h3>{ this.state.changedValue }</h3>
             </div>
         )
     }
