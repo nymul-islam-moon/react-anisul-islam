2db1b43e6478149525e328545cd282070e3f337b
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-12 11:07:56 +0600

    Class 20
    Prepare Project For Class 20

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import './index.css'; // CSS LINK UP
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
