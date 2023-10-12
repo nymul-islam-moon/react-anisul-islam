9f3eee18330bd93a420e71864aad14ce4359e717
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-12 13:10:18 +0600

    Class 21
    Login and Home Page loaded through cconditional Rendering

--- src/CONDITIONAL_RENDERING/index.js
+++ src/CONDITIONAL_RENDERING/index.js
@@ -1,10 +1,31 @@
 import React, {Component} from "react";
+import Home from './Home'
+import Login from './Login'
 
 class CONDITIONAL_RENDERING extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            isLoggedIn : true
+        }
+    }
+
     render() {
-        return (
-            <div>HI</div>
-        )
+
+        if ( this.state.isLoggedIn ) {
+            return <Home />
+        } else {
+            return <Login />
+        }
+
+        // return (
+        //     <div>
+        //
+        //     </div>
+        // )
     }
 }
 
