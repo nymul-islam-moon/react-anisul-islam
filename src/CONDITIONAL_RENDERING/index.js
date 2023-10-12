6b4e5575c9294e0a853628fc0faaa0db250ba6de
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-12 13:16:51 +0600

    Class 21
    ternary condition

--- src/CONDITIONAL_RENDERING/index.js
+++ src/CONDITIONAL_RENDERING/index.js
@@ -18,12 +18,7 @@ class CONDITIONAL_RENDERING extends Component {
         const { isLoggedIn } = this.state;
 
         let element;
-
-        if ( isLoggedIn ) {
-            element =  <Home />
-        } else {
-            element = <Login />
-        }
+        element = isLoggedIn ? <Home /> : <Login />
 
         return (
             /**
