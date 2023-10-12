f0c8e83a68d2de171cdf6b1ce852eba3121e3e4c
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-12 10:49:17 +0600

    Class 19
    React Bootstrap Card component used

--- src/components/React_Bootstrap/ReactBootstrap.js
+++ src/components/React_Bootstrap/ReactBootstrap.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const ReactBootstrap = () => {
-    return <div>
-        <h1>Card Title</h1>
-        <p>You need to install Node.js for local development. You can also choose to use Node.js in production, but you don’t have to. Many React frameworks support export to a static HTML/CSS/JS folder.</p>
-        <button>Lorem More</button>
-    </div>;
+    return <Card style={{width:"20rem"}}>
+        <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>You need to install Node.js for local development. You can also choose to use Node.js in production, but you don’t have to. Many React frameworks support export to a static HTML/CSS/JS folder.</Card.Text>
+            <Button className='btn btn-success'>Lorem More</Button>
+        </Card.Body>
+    </Card>;
 };
 
 export default ReactBootstrap;
