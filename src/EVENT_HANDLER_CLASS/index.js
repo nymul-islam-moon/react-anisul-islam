c01eddbd454eabcb478b447a9e53975164ae69a0
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-12 13:46:58 +0600

    Class 22
    On click Button checked

--- src/EVENT_HANDLER_CLASS/index.js
+++ src/EVENT_HANDLER_CLASS/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
 export default class EVENT_HANDLER_CLASS extends Component {
+
+    /**
+     * All Methods are implemented here before render
+     * @returns {JSX.Element}
+     */
+
+    handleClick = () => {
+        alert('Button Clicked')
+    }
+
     render() {
         return (
             <div>
-                Hello World
+                <button onClick={this.handleClick}> Click Here </button>
             </div>
         )
     }
