ea5f68029457835bbee7f577e4e4ed7da3ea8108
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-12 16:15:08 +0600

    Class 24
    Environment created for class 24

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import EVENT_BINDING from "./EVENT_BINDING/index";
+
 
 export default function App() {
     return (
         <div>
-            <EVENT_BINDING />
+            Hello
         </div>
     );
 }
