4924f95f47103f99f2ab975652ead9e81de349a8
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-13 10:36:37 +0600

    Class 26
    stopPropagation using 'event.stopPropagation()' for stop firing event with child method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import HOOKS_USESTATE2 from "./HOOKS_USESTATE/index2";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+
+    const handleChildClick = (event) => {
+        event.stopPropagation();
+        console.log( 'Child Event : ', event);
+    }
+
+    const handleParentClick = ( event ) => {
+        console.log( 'Parent Event : ',  event );
+    }
+
+
     return (
-        <div>
-            <HOOKS_USESTATE2 />
+        <div className="parent" onClick={handleParentClick}>
+            <h1>Welcome Every One</h1>
+            <button onClick={handleChildClick}>+</button>
         </div>
     );
 }
