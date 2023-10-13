c40390f26852d277f99e21efa4a9717f3c9685e5
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-13 12:27:24 +0600

    Class 28
    Calling Form component from App.js and style the FORM component using the form__class inside the FRM component and the css imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React, {useState} from 'react';
 import './style.css';
-
+import FORM from './components/FORM/FORM'
 export default function App() {
-
-    const [count, setCount] = useState(0);
-
-    const handleIncrement = (e) => {
-        setCount( count + 1 );
-    }
-
-    const handleDecrement = (e) => {
-        setCount( count - 1 );
-    }
-
-    const handleSetCount = (e) => {
-        setCount(0);
-    }
 
     return (
         <div className="counter center">
-            <h1 className="counter__title">Counter App</h1>
-            <div className="card center">
-                <h2 className="card__title">Count : { count }</h2>
-                <div className="card__btns">
-                    <button className="btn card__btn" onClick={handleIncrement} disabled={ count === 5 ? true : false }>+</button>
-                    <button className="btn card__btn" onClick={handleDecrement} disabled={ count === -5 ? true : false }>-</button>
-                    <button className="btn card__btn" onClick={handleSetCount} disabled={ count === 0 ? true : false }>0</button>
-                </div>
-            </div>
+            <FORM />
         </div>
     );
 }
---
+++ src/components/FORM/FORM.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FORM () {
+    return (
+        <div className="form__class">
+            I am from 1
+        </div>
+    )
+}
