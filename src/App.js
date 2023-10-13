a4f9491b4f60ebc262d1664036c62b456f45788c
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-13 17:06:29 +0600

    Class 30
    Passing data from child to parent using props

--- src/App.js
+++ src/App.js
@@ -11,9 +11,12 @@ import Child from "./components/STATE_LIFTING/Child";
 export default function App() {
 
     const data = "I am from Parent ( APP )";
+    const handleChildData = ( childData ) => {
+        console.log( "App : " + childData );
+    }
     return (
         <div className="counter center">
-            <Child data={data}/>
+            <Child data={data} onChildData={handleChildData}/>
         </div>
     );
 }
--- src/components/STATE_LIFTING/Child.js
+++ src/components/STATE_LIFTING/Child.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Child = (props) => {
-
+    const data = "I am from child component";
+    props.onChildData( data );
     return (
         <div>
             <p> I am Child Component </p>
