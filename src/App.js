4d0d76803a5fbb1201b2dd9253fe1b0f4a2c2034
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-13 11:56:54 +0600

    Class 27
    Assignment Done Counter Application Created and working properly

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 
 export default function App() {
 
-    const handleChildClick = (event) => {
-        event.stopPropagation();
-        console.log( 'Child Event : ', event);
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = (e) => {
+        setCount( count + 1 );
     }
 
-    const handleParentClick = ( event ) => {
-        console.log( 'Parent Event : ',  event );
+    const handleDecrement = (e) => {
+        setCount( count - 1 );
     }
 
+    const handleSetCount = (e) => {
+        setCount(0);
+    }
 
     return (
         <div className="counter center">
             <h1 className="counter__title">Counter App</h1>
             <div className="card center">
-                <h2 className="card__title">Count : 0</h2>
+                <h2 className="card__title">Count : { count }</h2>
                 <div className="card__btns">
-                    <button className="btn card__btn">+</button>
-                    <button className="btn card__btn">-</button>
-                    <button className="btn card__btn">0</button>
+                    <button className="btn card__btn" onClick={handleIncrement} disabled={ count === 5 ? true : false }>+</button>
+                    <button className="btn card__btn" onClick={handleDecrement} disabled={ count === -5 ? true : false }>-</button>
+                    <button className="btn card__btn" onClick={handleSetCount}>0</button>
                 </div>
             </div>
         </div>
