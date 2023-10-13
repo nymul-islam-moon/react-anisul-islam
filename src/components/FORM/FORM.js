8568af2a251874ae2df681d9fca80480dc2a9b13
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-13 15:15:26 +0600

    Class 29
    Use one method to perfome the form submition

--- src/components/FORM/FORM.js
+++ src/components/FORM/FORM.js
@@ -6,17 +6,21 @@ export default function FORM () {
     const [user, setUser] = useState({ name: '', email: '', password: '' })
     const {name, email, password}= user
 
+    const handleChange = (e) => {
+        const fieldName = e.target.name;
 
-    const handleNameChange = ( e ) => {
-        setUser({ name: e.target.value, email, password });
-    }
+        if ( 'name' === fieldName ) {
+            setUser({ name: e.target.value, email, password });
+        }
 
-    const handleEmailChange = ( e ) => {
-        setUser({ name, email: e.target.value, password });
-    }
+        if ( 'email' === fieldName ) {
+            setUser({ name, email: e.target.value, password });
+        }
 
-    const handlePasswordChange = ( e ) => {
-        setUser({ name, email, password: e.target.value} );
+        if ( 'password' === fieldName ) {
+            setUser({ name, email, password: e.target.value} );
+        }
+
     }
 
     const handleSubmit = (e) => {
@@ -30,15 +34,15 @@ export default function FORM () {
             <form action="" onSubmit={handleSubmit}>
                 <div className={style.formGroup}>
                     <label htmlFor="name">Name : </label>
-                    <input required type="text" name="name" id="name" value={name} onChange={handleNameChange}/>
+                    <input required type="text" name="name" id="name" value={name} onChange={handleChange}/>
                 </div>
                 <div className={style.formGroup}>
                     <label htmlFor="email">Email : </label>
-                    <input required type="email" name="email" id="email" value={email} onChange={handleEmailChange} />
+                    <input required type="email" name="email" id="email" value={email} onChange={handleChange} />
                 </div>
                 <div className={style.formGroup}>
                     <label htmlFor="password">Password : </label>
-                    <input required type="password" name="password" id="password" value={password} onChange={handlePasswordChange} />
+                    <input required type="password" name="password" id="password" value={password} onChange={handleChange} />
                 </div>
                 <button type="submit" >Register</button>
             </form>
