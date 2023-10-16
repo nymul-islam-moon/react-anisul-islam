71a8212672eeefaa56e9f8dbab6415368c70fb28
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-16 10:24:17 +0600

    CLass 32
    Set environment ofr class 32'

--- src/App.js
+++ src/App.js
@@ -6,21 +6,10 @@ import React from 'react';
 /**
  * Component Section
  */
-import Todo from "./Todo";
-import NewTodo from "./NewTodo";
-
 
 const App = () => {
-
-    const handleNewTodo = (newTodo) => {
-        console.log(newTodo);
-    }
-
      return (
-         <div>
-             <NewTodo onHandleNewTodo={handleNewTodo}/>
-               <Todo title={"I am ToDo"}/>
-         </div>
+         <div>I am APP</div>
      );
 };
 
