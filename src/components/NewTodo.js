9528b73b5ebede5c0e9e1fbabcb793b0b0918ffc
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-17 09:34:19 +0600

    Form Data shown in console log

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -6,22 +6,30 @@ const NewTodo = () => {
 
     const [todo, setTodo] = useState({title: "", desc: ""});
 
-    // const {}
+    const {title, desc} = todo;
+
+    const handleChange = ( event ) => {
+        const name = event.target.name;
+        setTodo( (oldTodo) => {
+            return { ...oldTodo, [name]: event.target.value }
+        } )
+    }
 
     const handleSubmit = ( event ) => {
         event.preventDefault();
+        console.log(todo);
     }
 
     return (
         <form className={Style.form} onSubmit={handleSubmit}>
             <div className={Style["form-field"]}>
                 <label htmlFor="title">Title: </label>
-                <input type="text" id="title" name="title" value={todo.title} />
+                <input type="text" id="title" name="title" value={ title } onChange={ handleChange } />
             </div>
 
             <div className={Style["form-field"]}>
                 <label htmlFor="desc">Description: </label>
-                <textarea type="text" id="desc" name="desc" value={todo.desc} />
+                <textarea type="text" id="desc" name="desc" value={ desc } onChange={ handleChange } />
             </div>
             <button type="submit">Add Todo</button>
 
