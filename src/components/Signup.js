7b23c7770f7af904eef063b71f511f7d538bf3e0
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-17 14:24:08 +0600

    Formik form submitted and consol log values

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,63 +11,35 @@ const Signup = () => {
             email: '',
             address: '',
             password: ''
+        },
+        onSubmit: ( values, { resetForm } ) => {
+            console.log( values );
+            resetForm({ values: "" });
         }
     })
 
-    /**
-     * The replacement of these lines are in the formik const into the initialValues part
-     * @param e
-     */
-    // const [name, setName] = useState("");
-    // const [email, setEmail] = useState("");
-    // const [phone, setPhone] = useState("");
-    // const [address, setAddress] = useState("");
-    // const [password, setPassword] = useState("");
-
-    const handleNameChange = (e) => {
-        setName( e.target.value );
-    }
-    const handlePhoneChange = (e) => {
-        setPhone( e.target.value );
-    }
-    const handleEmailChange = (e) => {
-        setEmail( e.target.value );
-    }
-    const handleAddressChange = (e) => {
-        setAddress( e.target.value );
-    }
-    const handlePasswordChange = (e) => {
-        setPassword( e.target.value );
-    }
-    const handleSubmit = ( e ) => {
-        e.preventDefault();
-        const newUser = {
-            name, email, phone, address, password
-        }
-        console.log( newUser);
-    }
     return<div>
         <h2>User Signup</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={ formik.handleSubmit }>
             <div>
                 <label htmlFor='name'>Name</label>
-                <input type='text' name='name' id='name' placeholder='Enter your name' onChange={ handleNameChange } value={name} />
+                <input type='text' name='name' id='name' placeholder='Enter your name' onChange={ formik.handleChange } value={formik.values.name} />
             </div>
             <div>
                 <label htmlFor='phone'>Phone</label>
-                <input type='text' name='phone' id='phone' placeholder='Enter your phone' onChange={ handlePhoneChange } value={ phone } />
+                <input type='text' name='phone' id='phone' placeholder='Enter your phone' onChange={ formik.handleChange } value={ formik.values.phone } />
             </div>
             <div>
                 <label htmlFor='email'>E-mail</label>
-                <input type='email' name='email' id='email' placeholder='Enter your email' onChange={ handleEmailChange } value={ email } />
+                <input type='email' name='email' id='email' placeholder='Enter your email' onChange={ formik.handleChange } value={ formik.values.email } />
             </div>
             <div>
                 <label htmlFor='email'>Address</label>
-                <input type='text' name='address' id='address' placeholder='Enter your address' onChange={ handleAddressChange } value={ address } />
+                <input type='text' name='address' id='address' placeholder='Enter your address' onChange={ formik.handleChange } value={ formik.values.address } />
             </div>
             <div>
                 <label htmlFor='password'>Password</label>
-                <input type='password' name='password' id='password' placeholder='Enter your password' onChange={ handlePasswordChange } value={ password } />
+                <input type='password' name='password' id='password' placeholder='Enter your password' onChange={ formik.handleChange } value={ formik.values.password } />
             </div>
             <button type='submit'>Signup</button>
         </form>
