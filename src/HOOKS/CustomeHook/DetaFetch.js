4270a4f1dd3b5c1c0df9b87d600220695bcc9297
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-18 13:50:12 +0600

    Class 42
    Custome hook added and fetch data

--- src/HOOKS/CustomeHook/DetaFetch.js
+++ src/HOOKS/CustomeHook/DetaFetch.js
@@ -1,41 +1,22 @@
 import react, { useEffect, useState } from "react";
+import useFetch from "./useFetch";
 
 const DataFetch = () => {
 
-    const [ todos, setTodos ] = useState( null );
-    const [ isLoading, setIsLoading ] = useState( true );
-    const [ error, setError ] = useState( null );
+    const { data, isLoading, error } = useFetch( "https://jsonplaceholder.typicode.com/todos" );
 
-    useEffect( () => {
-        fetch( "https://jsonplaceholder.typicode.com/todos" )
-            .then( ( res ) => {
-                if ( !res.ok ) {
-                    throw Error( 'Fetching is not successful' );
-                }
-                return res.json();
-            } )
-            .then( ( data ) => {
-                setTodos( data );
-                setIsLoading( false );
-                setError( null );
-            } )
-            .catch( ( error )=>{
-                setError( error.message )
-                setIsLoading( false );
-            } );
-    }, [] );
+    const loadingMessage = <p style={ { color:"greenyellow" } }>Data is loading...</p>;
+    const errorMessage = <p style={ { color: "red" } }>{ error }</p>;
 
-    const todosElement = todos && todos.map( ( todo ) => {
+    const todosElement = data && data.map( ( todo ) => {
         return <p key={ todo.id }> { todo.title } </p>
     });
-
-    const loadingMesage = <p style={ { color:"greenyellow" } }>Data is loading...</p>;
 
     return (
         <div>
             <h1>Todos</h1>
-            { isLoading && loadingMesage }
-            { error && <p style={ { color: "red" } }>{ error }</p> }
+            { isLoading && loadingMessage }
+            { error && errorMessage }
             { todosElement }
         </div>
     )
---
+++ src/HOOKS/CustomeHook/useFetch.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from "react";
+
+const useFetch = ( url ) => {
+    const [ data, setData ] = useState( null );
+    const [ isLoading, setIsLoading ] = useState( true );
+    const [ error, setError ] = useState( null );
+
+    useEffect( () => {
+        fetch( url )
+            .then( ( res ) => {
+                if ( ! res.ok ) {
+                    throw Error( 'Fetching is not successful' );
+                }
+                return res.json();
+            } )
+            .then( ( data ) => {
+                setData( data );
+                setIsLoading( false );
+                setError( null );
+            } )
+            .catch( ( error )=>{
+                setError( error.message )
+                setIsLoading( false );
+            } );
+    }, [url] );
+
+    return { data, isLoading, error  };
+
+}
+export default useFetch;
