65916f310de9f02f088d128e87d35a6de02a0be0
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-18 13:59:11 +0600

    React Toastify added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import React, { useState, useEffect } from 'react';
  * Component Section | main branch
  */
 import Style from './style.css';
-import DataFetch from "./HOOKS/CustomeHook/DetaFetch";
+
 
 const App = () => {
      return (
          <div>
-             <DataFetch />
+            <h1>APP</h1>
          </div>
      );
 };
