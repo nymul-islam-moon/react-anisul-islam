7b36d25bfbce701e2805ecfea7f2b06a2e9a3534
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-18 14:59:19 +0600

    React Toastify used

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
  * Third Party Section
  */
 import React, { useState, useEffect } from 'react';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 /**
  * Component Section | main branch
@@ -9,10 +11,23 @@ import React, { useState, useEffect } from 'react';
 import Style from './style.css';
 
 
+
 const App = () => {
+
+    const handleAddNewTodo = () => {
+        toast('New Todo Added');
+    }
+
+    const handleDeleteTodo = () => {
+        toast('Todo Deleted');
+    }
+
      return (
          <div>
-            <h1>APP</h1>
+            <h1>TODO APP</h1>
+             <button onClick={handleAddNewTodo} >Add New Todo</button>
+             <button onClick={handleDeleteTodo} >Delete Todo</button>
+             <ToastContainer />
          </div>
      );
 };
