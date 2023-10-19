ef8c172ee03676ba8ef49ccad83b22730822e741
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-19 17:38:39 +0600

    Dynamic Style Added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-/**
- * Third Party Section
- */
 import React, { useState, useEffect } from 'react';
-import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
-/**
- * Component Section | main branch
- */
 import Style from './style.css';
 
-
-
 const App = () => {
+    
+    const [name, setName] = useState("");
+    const [ validInput, setValidInput ] = useState(true);
 
-    const handleAddNewTodo = () => {
-        toast('New Todo Added');
-    }
+    useEffect(() => {
+        if (name.length < 2) {
+            setValidInput(false);
+        } else {
+            setValidInput(true);
+        }
+    }, [name]);
 
-    const handleDeleteTodo = () => {
-        toast('Todo Deleted');
-    }
 
-     return (
-         <div>
-            <h1>TODO APP</h1>
-             <button onClick={handleAddNewTodo} >Add New Todo</button>
-             <button onClick={handleDeleteTodo} >Delete Todo</button>
-             <ToastContainer />
-         </div>
-     );
+    const handleChange = ( e ) => {
+        setName( e.target.value );
+
+    };
+
+    return (
+        <div className="container">
+            <input type="text" value={name} onChange={handleChange} className={`${validInput ? 'valid' : 'invalid'}`} />
+        </div>
+    );
 };
 
 export default App;
