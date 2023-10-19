7a4a0580cdfc159a9a19c52ad10b25e9e9399076
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-19 18:03:32 +0600

    Class 48
    Prepare environment for the class

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import Style from './style.css';
 
 const App = () => {
     
-    const [name, setName] = useState("");
-    const [ validInput, setValidInput ] = useState(true);
-
-    useEffect(() => {
-        if (name.length < 2) {
-            setValidInput(false);
-        } else {
-            setValidInput(true);
-        }
-    }, [name]);
-
-
-    const handleChange = ( e ) => {
-        setName( e.target.value );
-
-    };
 
     return (
-        <div className="container">
-            <input type="text" value={name} onChange={handleChange} className={`${validInput ? 'valid' : 'invalid'}`} />
-        </div>
+        <div>Hello</div>
     );
 };
 
