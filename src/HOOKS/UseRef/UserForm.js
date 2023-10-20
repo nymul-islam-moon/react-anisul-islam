a432f33439887833a065787be7c7e7789c6f96c9
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-20 16:00:33 +0600

    Class 52
    User name and password printed

--- src/HOOKS/UseRef/UserForm.js
+++ src/HOOKS/UseRef/UserForm.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, {useRef} from "react";
 
 const UserForm = () => {
 
+    const userNameRef = useRef();
+    const passwordRef = useRef();
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log('submitted');
+        const userName = userNameRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(userName);
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="userName">User Name : </label>
-                <input type="text" id="userName" />
+                <input type="text" id="userName" ref={userNameRef} />
             </div>
 
             <div>
                 <label htmlFor="password">Password : </label>
-                <input type="password" id="password" />
+                <input type="password" id="password" ref={passwordRef} />
             </div>
             <button type="submit">Register</button>
         </form>
