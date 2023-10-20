f84c6d73348eb3259400674756f6769179e7df56
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-20 12:07:05 +0600

    Class 50
    Mounting example

--- src/Components/LifeCycle/LifeCycle.js
+++ src/Components/LifeCycle/LifeCycle.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 
+// Mounting -> constructor -> render -> componentDidMount
+// Updating -> state/props -> render ->
+// Unmounting
+
 export default class LifeCycle extends Component {
 
     constructor(props) {
         super(props);
         console.log("constructor");
-        this.state = {
-
-        }
+        this.state = {};
     }
+    componentDidMount() {
+        console.log("ComponentDidMount");
+    }
+
     render() {
         {
             console.log('render');
