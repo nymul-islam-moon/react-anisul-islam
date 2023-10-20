62a56672b559ff7542ae26dfd1d66daa5d15bf62
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-20 12:51:14 +0600

    Class 50
    Update LifeCycle in console log

--- src/Components/LifeCycle/LifeCycle.js
+++ src/Components/LifeCycle/LifeCycle.js
@@ -9,10 +9,28 @@ export default class LifeCycle extends Component {
     constructor(props) {
         super(props);
         console.log("constructor");
-        this.state = {};
+        this.state = {
+            count: 0
+        };
     }
+
+    shouldComponentUpdate() {
+        console.log("shouldComponentUpdate");
+        return true;
+    }
+
     componentDidMount() {
         console.log("ComponentDidMount");
+    }
+
+    componentDidUpdate() {
+        console.log('ComponentDidUpdate');
+    }
+
+    handleIncrement = () => {
+        this.setState({
+            count : this.state.count + 1
+        })
     }
 
     render() {
@@ -21,7 +39,10 @@ export default class LifeCycle extends Component {
         }
         return (
 
-            <div>LifeCycle</div>
+            <div>
+                <h1>Counter : {this.state.count}</h1>
+                <button onClick={this.handleIncrement}>+</button>
+            </div>
         )
     }
 }
