215d380bea1b59b3d48ed8a0c3ae8bf01c7c9f53
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-20 15:03:50 +0600

    Class 51
    Using ref and direct manupulate dom done

--- src/Components/LifeCycle/RefExample/UserForm.js
+++ src/Components/LifeCycle/RefExample/UserForm.js
@@ -1,9 +1,33 @@
-import React, { Component } from "react";
+import React, {Component, createRef} from "react";
 
 export default class UserForm extends Component {
+
+    constructor(props) {
+        super(props)
+        this.userNameRef = createRef();
+        this.state = {}
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.userNameRef.current.value);
+        this.userNameRef.current.style.backgroundColor = "green";
+    }
+
     render() {
         return (
-            <div>UserForm</div>
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-field">
+                    <label htmlFor="userName">User Name : </label>
+                    <input type="text" id="user_name" autoComplete="false" ref={this.userNameRef} />
+                </div>
+
+                <div className="form-field">
+                    <label htmlFor="password">Password : </label>
+                    <input type="password" id="password" autoComplete="false" />
+                </div>
+                <button type="submit">Register</button>
+            </form>
         )
     }
 }
