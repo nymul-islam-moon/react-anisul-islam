1e1258342abe2998dbc82e7a8826fc3635c1d62a
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-25 16:57:33 +0600

    Class 66
    dynamic title added

--- src/App.js
+++ src/App.js
@@ -2,31 +2,16 @@
  * Import Third Party
  */
 import React, {useEffect, useState} from 'react';
-import axios from "axios";
+import {Helmet} from "react-helmet";
 
 const App = () => {
-
-    const [ users, setUsers ] = useState([]);
-
-    const getAllUsers = async () => {
-        const response = await axios.get('http://localhost:3001/users');
-        setUsers(response.data);
-    }
-
-    useEffect( () => {
-        getAllUsers();
-    }, [] )
-
     return (
-        <>
-            { users.map( user => {
-                return <article key={user.id}>
-                    <h2>{ user.name }</h2>
-                    <h3>{ user.email }</h3>
-                </article>
-            } ) }
-        </>
-
+        <div>
+            <Helmet>
+                <title>Home Page</title>
+            </Helmet>
+            <h3>APP</h3>
+        </div>
     );
 };
 
