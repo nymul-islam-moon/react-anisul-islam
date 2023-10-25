fee7ffaec2704f577dcdaf1b4ba7a6cd653c9487
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-25 11:32:59 +0600

    Class 62 Imortant
    Query Parameter all done

--- src/pages/User.js
+++ src/pages/User.js
@@ -3,18 +3,20 @@ import {useSearchParams} from "react-router-dom";
 
 const User = () => {
     const [ name, setName ] = useState("");
+    const [ age, setAge ] = useState("");
     const [ searchParams, setSearchParams ] = useSearchParams();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setSearchParams({name: name});
+        setSearchParams({ name: name, age: age });
     }
 
     return (
         <div>
             <h1>User : </h1>
             <form onSubmit={handleSubmit}>
-                <input type="text" value={name} onChange={(e) => { setName(e.target.value) }}/>
+                <input type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="User Name"/>
+                <input type="number" value={age} onChange={(e) => { setAge(e.target.value) }} placeholder="User Age"/>
                 <button type="submit">Find User</button>
             </form>
         </div>
