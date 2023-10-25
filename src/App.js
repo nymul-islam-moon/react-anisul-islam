0457dfab02e4677dffb52dda031c153cf3be3fac
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-25 16:36:14 +0600

    Class 65
    Json server created and data fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 /**
  * Import Third Party
  */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const App = () => {
+
+    const [ users, setUsers ] = useState([]);
+
+    const getAllUsers = async () => {
+        const response = await axios.get('http://localhost:3001/users');
+        setUsers(response.data);
+    }
+
+    useEffect( () => {
+        getAllUsers();
+    }, [] )
+
     return (
         <>
-           APP
+            { users.map( user => {
+                return <article key={user.id}>
+                    <h2>{ user.name }</h2>
+                    <h3>{ user.email }</h3>
+                </article>
+            } ) }
         </>
 
     );
