4b2d156d7abcf996e43ef3f471a3ae5646c332a7
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-25 15:31:03 +0600

    Refresh The Project

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@
  * Import Third Party
  */
 import React from 'react';
-import style from './style.css';
-import Home from "./pages/Home";
-import About from "./pages/About";
-
 
 const App = () => {
     return (
         <>
-            App uses Routes
-            <Home />
-            <About />
+           APP
         </>
 
     );
