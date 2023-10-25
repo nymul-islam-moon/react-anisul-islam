735d7adfc6cdcdec04adfc06ba58b737028db7b2
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-25 11:05:21 +0600

    Class 62
    Query Parameter

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,11 +1,14 @@
 import React from "react";
-import {useParams} from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const User = () => {
    const { userId } = useParams();
+   const [ searchParams, setSearchParams ] = useSearchParams();
+   // console.log( searchParams.get("id") );
     return (
         <div>
-            <h1>User : {userId}</h1>
+            <h1>User : </h1>
+            <h3>{ searchParams.get("id") }</h3>
         </div>
     );
 };
