42f0804ad382ccb952357448a65b243174d26f6e
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-26 09:29:59 +0600

    Class 69
    Pending..

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@
  * Import Third Party
  */
 import React, {useEffect, useState} from 'react';
-import {Helmet} from "react-helmet";
 
 const App = () => {
     return (
         <div>
-            <Helmet>
-                <title>Home Page</title>
-            </Helmet>
             <h3>APP</h3>
         </div>
     );
