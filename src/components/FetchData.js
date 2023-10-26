52f8ae6d46ea94c38b29fe5ec1807cf9d8685860
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-26 17:07:32 +0600

    Wordpress APP
    Form created validation added, data fatching from fetchdata age dummy data shown

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const FetchData = () => {
-    return 'hi';
+
+    const data = [
+        {
+            id: 1,
+            name: 'Nymul Islam',
+            phone: '01786283389',
+            email: 'towkir1997islam@gmail.com',
+            address: 'Mohammadpur, Dhaka',
+        },
+        {
+            id: 2,
+            name: 'Towkir Islam',
+            phone: '01456283389',
+            email: 'nymul.islam@gmail.com',
+            address: 'Dhanmondi, Dhaka',
+        }
+    ];
+
+    return data;
 };
 
 export default FetchData;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,18 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import Contacts from "./Contacats";
 import Create from "./Create";
+import FetchData from "./FetchData";
 
-const dummyUsers = [
-    {
-        id: 1,
-        name: 'Nymul Islam',
-        phone: '01786283389',
-        email: 'towkir1997islam@gmail.com',
-        address: 'Mohammadpur, Dhaka',
-    },
-    {
-        id: 2,
-        name: 'Towkir Islam',
-        phone: '01786287789',
-        email: 'nymul.islam@gmail.com',
-        address: 'Dhaka, Bangladesh'
-    },
-    {
-        id: 3,
-        name: 'Arif Islam',
-        email: 'arif@gmail.com',
-        phone: '01839456456',
-        address: 'Chittagong, Bangladesh'
-    },
-    {
-        id: 4,
-        name: 'Arif Islam',
-        email: 'arif@gmail.com',
-        phone: '01839456456',
-        address: 'Chittagong, Bangladesh'
-    }
-];
 
 const Home = () => {
+
+    const contacts = FetchData();
+
     return (
         <div className="container">
             <h1 style={{ color:"white" }}>Contacts List</h1>
             <Create />
-            <Contacts contacts={dummyUsers}/>
+            <Contacts contacts={contacts}/>
         </div>
     );
 };
